d405c08e735631d4eba224ff9d5518387c5e75a3
dahuangggg <dahuangggg@icloud.com>
2023-11-04 17:08:26 +0800

    feat(Views): Improve UI.
    1. Managers can now control the ACs from their page.
    2. Receptionists
    can now see the ACs from their page.
    3. Add a navigation bar.

--- frontend/src/views/ManagerView.tsx
+++ frontend/src/views/ManagerView.tsx
@@ -1,78 +1,132 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import {
-  Container,
-  Typography,
-  Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Card,
-  CardContent
+    Container,
+    Typography,
+    ButtonGroup,
+    Button,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Card,
+    CardContent,
+    Select,
+    SelectChangeEvent,
+    MenuItem,
 } from "@mui/material";
-import { useAppDispatch } from "../store";
-import { useSelector, useDispatch } from "react-redux";
-import {fetchAcInfos, getAcInfo, getAcInfos} from "../slices/authSlice";
-import { useNavigate } from "react-router-dom";
+import {useAppDispatch} from "../store";
+import {useSelector, useDispatch} from "react-redux";
+import {fetchAcInfos, getAcInfo, getAcInfos, updateAcInfo} from "../slices/authSlice";
+import {useNavigate, Link} from "react-router-dom";
+// import { increaseTemperature, decreaseTemperature, toggleAcStatus, handleAcModeChange } from './CustomerAcView';
+import NavigationBar from "./NavigationBar";
 
 const AdminAcView: React.FC = () => {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  
-  const allAcInfos = useSelector(getAcInfos);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    dispatch(fetchAcInfos());
-  }, [dispatch]);
+    const allAcInfos = useSelector(getAcInfos);
 
-  return (
-    <Container>
-      <Typography variant="h4" align="center" gutterBottom>
-        管理员空调信息
-      </Typography>
-      <Card elevation={3}>
-        <CardContent>
-          <TableContainer>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>房间号</TableCell>
-                  <TableCell>当前温度</TableCell>
-                  <TableCell>目标温度</TableCell>
-                  <TableCell>状态</TableCell>
-                  <TableCell>操作</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {allAcInfos.map((acInfo, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{acInfo.roomNumber}</TableCell>
-                    <TableCell>{acInfo.currentTemperature}°C</TableCell>
-                    <TableCell>{acInfo.targetTemperature}°C</TableCell>
-                    <TableCell>{acInfo.acStatus ? "开启" : "关闭"}</TableCell>
-                    <TableCell>
-                      <Button
-                        variant="outlined"
-                        color="primary"
-                        onClick={() => {
-                          navigate(`/ac-control/${acInfo.roomNumber}`);
-                        }}
-                      >
-                      控制
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </CardContent>
-      </Card>
-    </Container>
-  );
+    useEffect(() => {
+        dispatch(fetchAcInfos());
+    }, [dispatch]);
+
+
+    const getAcInfoByRoomNumber = (roomNumber: string) => {
+        return allAcInfos.find((acInfo) => acInfo.roomNumber === roomNumber);
+    };
+
+    const handleAcStatusToggle = (roomNumber: string) => {
+        const acInfo = getAcInfoByRoomNumber(roomNumber);
+        if (!acInfo) return; // 如果没有找到对应的信息，直接返回
+
+        const newAcStatus = !acInfo.acStatus;
+        dispatch(updateAcInfo(acInfo.targetTemperature, newAcStatus, acInfo.acMode));
+    }
+
+    const handleModeChange = (roomNumber: string, e: SelectChangeEvent<string>) => {
+        const acInfo = getAcInfoByRoomNumber(roomNumber);
+        if (!acInfo) return; // 如果没有找到对应的信息，直接返回
+
+        const newAcMode = e.target.value as string;
+        dispatch(updateAcInfo(acInfo.targetTemperature, acInfo.acStatus, newAcMode));
+    }
+
+    const handleTemperatureChange = async (roomNumber: string, delta: number) => {
+        const acInfo = getAcInfoByRoomNumber(roomNumber);
+        if (!acInfo) return; // 如果没有找到对应的信息，直接返回
+
+        const newTargetTemperature = (acInfo.targetTemperature || 0) + delta;
+        await dispatch(updateAcInfo(newTargetTemperature, acInfo.acStatus, acInfo.acMode));
+    }
+
+    return (
+        <>
+            <NavigationBar/>
+            <Container>
+                <Typography variant="h4" align="center" gutterBottom style={{ paddingTop: "16px", paddingBottom: "16px"}}>
+                    管理员空调信息
+                </Typography>
+                <Card elevation={3}>
+                    <CardContent>
+                        <TableContainer>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>房间号</TableCell>
+                                        <TableCell>当前温度</TableCell>
+                                        <TableCell>目标温度</TableCell>
+                                        <TableCell>风速</TableCell>
+                                        <TableCell>状态</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {allAcInfos.map((acInfo) => (
+                                        <TableRow key={acInfo.roomNumber}>
+                                            <TableCell>{acInfo.roomNumber}</TableCell>
+                                            <TableCell>{acInfo.currentTemperature}°C</TableCell>
+                                            <TableCell>
+                                                <ButtonGroup size="small" aria-label="small outlined button group">
+                                                    <Button
+                                                        onClick={() => handleTemperatureChange(acInfo.roomNumber, -1)}>-</Button>
+                                                    <Button disabled>{acInfo.targetTemperature}°C</Button>
+                                                    <Button
+                                                        onClick={() => handleTemperatureChange(acInfo.roomNumber, 1)}>+</Button>
+                                                </ButtonGroup>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Select
+                                                    value={acInfo.acMode}
+                                                    onChange={(e) => handleModeChange(acInfo.roomNumber, e)}
+                                                    displayEmpty
+                                                    size="small"
+                                                >
+                                                    <MenuItem value="低风速">低风速</MenuItem>
+                                                    <MenuItem value="中风速">中风速</MenuItem>
+                                                    <MenuItem value="高风速">高风速</MenuItem>
+                                                </Select>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Button
+                                                    variant="contained"
+                                                    color={acInfo.acStatus ? "primary" : "secondary"}
+                                                    onClick={() => handleAcStatusToggle(acInfo.roomNumber)}
+                                                >
+                                                    {acInfo.acStatus ? "关闭" : "打开"}
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </CardContent>
+                </Card>
+            </Container>
+        </>
+    );
 };
 
 export default AdminAcView;
-
--- frontend/src/views/ReceptionView.tsx
+++ frontend/src/views/ReceptionView.tsx
@@ -1,4 +1,4 @@
-// 测试完成!
+import React, {useEffect} from "react";
 import {
     Container,
     Typography,
@@ -10,44 +10,129 @@ import {
     TableHead,
     TableRow,
     Card,
-    CardContent,
-    Box,
+    CardContent
 } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import { fetchAcInfos, getAcInfo, getAcInfos } from "../slices/authSlice";
-import { useAppDispatch } from "../store";
-import axios from "axios";
-import { useSelector } from "react-redux";
+import {useAppDispatch} from "../store";
+import {useSelector, useDispatch} from "react-redux";
+import {fetchAcInfos, getAcInfo, getAcInfos} from "../slices/authSlice";
+import {useNavigate} from "react-router-dom";
+import NavigationBar from "./NavigationBar";
 
-const ReceptionView: React.FC = () => {
-  const dispatch = useAppDispatch();
-  //   const [acInfoData, setAcInfoData] = useState(null); // 这里存储从fetchAcInfo获取的数据
-  const allAcInfos = useSelector(getAcInfos);
+const AdminAcView: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    // 这是一个假设。实际上，fetchAcInfo函数应该返回数据，但在给出的代码中它没有返回。
-    // 因此，请根据实际情况修改这部分代码。
-    const fetchData = async () => {
-      dispatch(fetchAcInfos());
-      //   setAcInfoData(data.data);
-      //   console.log(data.data);
-    };
+    const allAcInfos = useSelector(getAcInfos);
 
-    fetchData();
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(fetchAcInfos());
+    }, [dispatch]);
 
-  return (
-    <Container>
-      <Typography variant="h4" align="center" gutterBottom>
-        测试fetchAcInfos
-      </Typography>
-      <Box mt={3}>
-        <Typography>
-          acInfo: {allAcInfos ? JSON.stringify(allAcInfos) : "加载中..."}
-        </Typography>
-      </Box>
-    </Container>
-  );
+    return (
+        <>
+            <NavigationBar/>
+            <Container>
+                <Typography variant="h4" align="center" gutterBottom style={{ paddingTop: "16px", paddingBottom: "16px"}}>
+                    管理员空调信息
+                </Typography>
+                <Card elevation={3}>
+                    <CardContent>
+                        <TableContainer>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>房间号</TableCell>
+                                        <TableCell>当前温度</TableCell>
+                                        <TableCell>目标温度</TableCell>
+                                        <TableCell>风速</TableCell>
+                                        <TableCell>状态</TableCell>
+                                        <TableCell>详情</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {allAcInfos.map((acInfo, index) => (
+                                        <TableRow key={index}>
+                                            <TableCell>{acInfo.roomNumber}</TableCell>
+                                            <TableCell>{acInfo.currentTemperature}°C</TableCell>
+                                            <TableCell>{acInfo.targetTemperature}°C</TableCell>
+                                            <TableCell>{acInfo.acMode}</TableCell>
+                                            <TableCell>{acInfo.acStatus ? "开启" : "关闭"}</TableCell>
+                                            <TableCell>
+                                                <Button
+                                                    variant="outlined"
+                                                    color="primary"
+                                                    onClick={() => {
+                                                        navigate(`/ac-control/${acInfo.roomNumber}`);
+                                                    }}
+                                                >
+                                                    查看
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </CardContent>
+                </Card>
+            </Container>
+        </>
+    );
 };
 
-export default ReceptionView;
+export default AdminAcView;
+
+//
+// // 测试完成!
+// import {
+//     Container,
+//     Typography,
+//     Button,
+//     Table,
+//     TableBody,
+//     TableCell,
+//     TableContainer,
+//     TableHead,
+//     TableRow,
+//     Card,
+//     CardContent,
+//     Box,
+// } from "@mui/material";
+// import React, { useEffect, useState } from "react";
+// import { fetchAcInfos, getAcInfo, getAcInfos } from "../slices/authSlice";
+// import { useAppDispatch } from "../store";
+// import axios from "axios";
+// import { useSelector } from "react-redux";
+//
+// const ReceptionView: React.FC = () => {
+//   const dispatch = useAppDispatch();
+//   //   const [acInfoData, setAcInfoData] = useState(null); // 这里存储从fetchAcInfo获取的数据
+//   const allAcInfos = useSelector(getAcInfos);
+//
+//   useEffect(() => {
+//     // 这是一个假设。实际上，fetchAcInfo函数应该返回数据，但在给出的代码中它没有返回。
+//     // 因此，请根据实际情况修改这部分代码。
+//     const fetchData = async () => {
+//       dispatch(fetchAcInfos());
+//       //   setAcInfoData(data.data);
+//       //   console.log(data.data);
+//     };
+//
+//     fetchData();
+//   }, [dispatch]);
+//
+//   return (
+//     <Container>
+//       <Typography variant="h4" align="center" gutterBottom>
+//         测试fetchAcInfos
+//       </Typography>
+//       <Box mt={3}>
+//         <Typography>
+//           acInfo: {allAcInfos ? JSON.stringify(allAcInfos) : "加载中..."}
+//         </Typography>
+//       </Box>
+//     </Container>
+//   );
+// };
+//
+// export default ReceptionView;
