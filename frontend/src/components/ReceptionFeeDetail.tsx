1d07cc028c6d950e09c9865da88c4acf17b24d06
李根赢 <115919719+GHOST-LOVE-YOU@users.noreply.github.com>
2023-11-10 11:06:50 +0800

    QAQ

---
+++ frontend/src/components/ReceptionFeeDetail.tsx
@@ -0,0 +1,10 @@
+//  写一个简单的return
+const ReceptionFeeDetail = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>ReceptionFeeDetail</h1>
+    </div>
+  );
+};
+
+export default ReceptionFeeDetail;
